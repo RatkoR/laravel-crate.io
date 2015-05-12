0f365f32960b5f8851847be4b63d2feb8ed75c2e
Ratko Rudic <ratko@kramfid.org>
2015-05-12 14:55:32 +0200

    Fix binding array and object values.
    Crate's pdo execute() produces an error if you try to bind
    array (or object) values. To get arround this error, we have
    to bind all values one at the time.

--- src/RatkoR/Crate/Connection.php
+++ src/RatkoR/Crate/Connection.php
@@ -4,6 +4,7 @@ use RatkoR\Crate\Schema\Builder;
 use Crate\DBAL\Driver\PDOCrate\Driver as DoctrineDriver;
 use RatkoR\Crate\Query\Grammars\Grammar as QueryGrammar;
 use RatkoR\Crate\Schema\Grammars\Grammar as SchemaGrammar;
+use Crate\PDO\PDO;
 
 class Connection extends \Illuminate\Database\Connection
 {
@@ -76,4 +77,75 @@ class Connection extends \Illuminate\Database\Connection
 		return new DoctrineDriver;
 	}
 
+	/**
+	 * Execute an SQL statement and return the boolean result.
+	 * 
+	 * We need to override statement to bind parameters one by one. The
+	 * execute() function of crate-pdo can take bind parameters, but it
+	 * wrongly sets all of them to strings. So if you bind array to an
+	 * array field, you'd get error. A patch was sent to crate-pdo, but was
+	 * NACK-ed (with "fix eloquent").
+	 * 
+	 * So in order to make this work we override statement().
+	 *
+	 * @param  string  $query
+	 * @param  array   $bindings
+	 * @return bool
+	 */
+	public function statement($query, $bindings = array())
+	{
+		return $this->run($query, $bindings, function($me, $query, $bindings)
+		{
+			if ($me->pretending()) return true;
+
+			$bindings = $me->prepareBindings($bindings);
+
+			$stmt = $me->getPdo()->prepare($query);
+			
+			$this->bindParameters($stmt, $bindings);
+	
+			return $stmt->execute();
+		});
+	}
+	
+	protected function bindParameters(&$stmt, $values)
+	{
+		if (!$values || !is_array($values))
+			return;
+
+		$i = 0;
+		
+		foreach ($values AS $value) {
+			$this->bindParameter($i++, $stmt, $value);
+		}
+	}
+	
+	protected function bindParameter($index, &$stmt, $value)
+	{
+		$dataType = $this->guessDataType($value);
+		$stmt->bindParam($index+1, $value, $dataType);
+	}
+	
+    /**
+     * Returns PDO::PARAM_* type based on parameter value.
+     */
+    protected function guessDataType($value) 
+    {
+        switch (gettype($value)) {
+            case 'array':
+                return PDO::PARAM_ARRAY;
+            case 'object':
+                return PDO::PARAM_OBJECT;
+            case 'double':
+                return PDO::PARAM_DOUBLE;
+            case 'boolean':
+                return PDO::PARAM_BOOL;
+            case 'NULL':
+                return PDO::PARAM_NULL;
+            case 'integer':
+                return PDO::PARAM_LONG;
+            default:
+                return PDO::PARAM_STR;
+        }
+    }	
 }
