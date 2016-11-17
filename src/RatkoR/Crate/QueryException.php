a1f99fb08122317a574294406fafac59f4fc7405
Ratko Rudic <ratko.rudic@pop-tv.si>
2016-11-17 19:34:23 +0100

    Correctly print array and object SQL parameters in case of an exception.
    Laravel tries to show SQL that caused an exception. While doing it
    it also prints all SQL parameters. If there are arrays or objects
    in parameter it throws an "Object of class stdClass
    could not be converted to string" exception.
    
    This commit json_encodes all non scalar parameters so that they can
    be printed in exception.

---
+++ src/RatkoR/Crate/QueryException.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace RatkoR\Crate;
+
+use Illuminate\Database\QueryException as BaseQueryException;
+
+class QueryException extends BaseQueryException
+{
+
+    /**
+     * Format the SQL error message.
+     * 
+     * Overriden to support extended types like arrays and objects.
+     *
+     * @param  string  $sql
+     * @param  array  $bindings
+     * @param  \Exception $previous
+     * @return string
+     */
+    protected function formatMessage($sql, $bindings, $previous)
+    {
+        $stringCasted = $this->valuesToStrings($bindings);
+
+        return $previous->getMessage().' (SQL: '.Str::replaceArray('?', $bindings, $sql).')';
+    }
+
+    /**
+     * Non scalar fields are json_encoded to string values.
+     */
+    protected function valuesToStrings($bindings)
+    {
+        foreach ($bindings as $key => $binding) {
+            $bindings[$key] = is_scalar($binding) ?
+                                $binding : json_encode($binding);
+        }
+
+        return $bindings;
+    }
+}
